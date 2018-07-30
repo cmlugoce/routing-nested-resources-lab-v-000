a1f42678b3aca88b56c9d83a660434550d8285aa
Cristina Lugo <cmlugoce@syr.edu>
2018-07-30 00:05:31 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,16 @@ class SongsController < ApplicationController
 use Rack::Flash
 
   def index
-    @songs = Song.all
+    if params[:artist_id]
+     @artist = Artist.find_by(id: params[:artist_id])
+     if @artist.nil?
+       redirect_to artists_path, alert: "Artist not found."
+     else
+       @songs = @artist.songs
+     end
+   else
+     @songs = Song.all
+   end
   end
 
   def show
